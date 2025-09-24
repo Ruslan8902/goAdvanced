a625fcca073f206f5f254544c84db054c8c8963d
Ruslan8902 <araslanov.r.v@yandex.ru>
2025-09-24 22:20:43 +0500

    task_1_update

--- 1-concurrency/main.go
+++ 1-concurrency/main.go
@@ -8,10 +8,8 @@ import (
 func main() {
 	ch_1 := make(chan int)
 	ch_2 := make(chan int)
-	for i := 0; i < 10; i++ {
-		go generate_random_n_to_chan(ch_1)
-		go pow_two_of_n_from_chan_to_chan(ch_1, ch_2)
-	}
+	go generate_random_n_to_chan(ch_1)
+	go pow_two_of_n_from_chan_to_chan(ch_1, ch_2)
 
 	for i := 0; i < 10; i++ {
 		n := <-ch_2
@@ -20,10 +18,14 @@ func main() {
 }
 
 func generate_random_n_to_chan(c chan int) {
-	c <- rand.Intn(101)
+	for i := 0; i < 10; i++ {
+		c <- rand.Intn(101)
+	}
 }
 
 func pow_two_of_n_from_chan_to_chan(c_from chan int, c_to chan int) {
-	n := <-c_from
-	c_to <- (n * n)
+	for i := 0; i < 10; i++ {
+		n := <-c_from
+		c_to <- (n * n)
+	}
 }
