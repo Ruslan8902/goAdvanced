d7c9334cbece7ed7f094b2fecff4b5ad16770510
Ruslan8902 <araslanov.r.v@yandex.ru>
2025-11-20 22:56:14 +0500

    m11

--- 4-order-api/cmd/main.go
+++ 4-order-api/cmd/main.go
@@ -1,21 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"orderApiStart/configs"
+	"orderApiStart/internal/product"
+	"orderApiStart/middleware"
 	"orderApiStart/pkg/db"
 )
 
-func main() {
+func App() http.Handler {
 	conf := configs.LoadConfig()
-	_ = db.NewDb(conf)
-
+	db := db.NewDb(conf)
 	router := http.NewServeMux()
 
+	productRepository := product.NewProductRepository(db)
+
+	product.NewProductHandler(router, product.ProductHandlerDeps{
+		ProductRepository: productRepository,
+		Config:            conf,
+	})
+	return middleware.Logging(router)
+}
+
+func main() {
+	app := App()
 	server := http.Server{
 		Addr:    ":8081",
-		Handler: router,
+		Handler: app,
 	}
-
+	fmt.Println("Server is listening on port 8081")
 	server.ListenAndServe()
 }
